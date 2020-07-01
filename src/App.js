5d96e5b122fa7ce12be08b0f536a275173a4dde4
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-06-30 21:55:42 -0700

    working odometer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header text="COVID lockdown"/>
+      <main>
+        <Odometer/>
+      </main>
+      <Footer trademark="Prasangam"/>
     </div>
   );
 }
+
+function Header(props){
+  return (
+      <header className="header">
+        <h1>
+          Counting the {props.text}
+        </h1>
+      </header>
+  )
+}
+
+function Footer(props){
+  return (
+    <footer>
+      @{props.trademark}
+    </footer>
+  )
+}
+
+class Odometer extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      day:1,
+    }
+  }
+  changeDays(change){
+    let newDay = '';
+    if (change === '1000'){
+      newDay = this.state.day + 1000
+    }
+    else if (change ==='100') {
+      newDay = this.state.day + 100
+    }
+    else if (change === '10'){
+      newDay = this.state.day + 10
+    }
+    else if (change === '1'){
+      newDay = this.state.day + 1
+    }
+    this.setState({
+      day: newDay,
+    })
+  }
+  render(){
+    return (
+      <>
+      <nav>
+      <button onClick={() => this.changeDays("1000")}>+1000</button>
+      <button onClick={() => this.changeDays("100")}>+100</button>
+      <button onClick={() => this.changeDays("10")}>+10</button>
+      <button onClick={() => this.changeDays("1")}>+1</button>
+      </nav>
+      <h2>{this.state.day} days </h2>
+      </>
+    )
+  }
+}
+
 
 export default App;
